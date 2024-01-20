6b3aa5bdf9fa75af064e16e792364d9318ac066a
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-01-20 17:28:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Ibrahim'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Ibrahim:ibrahim@cluster0.cpmhizg.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
+global.your_email = "api.lucifercloud@proton.me" //email
+global.email_password = "Ibrahim@123" //application password email
+global.limitCount = 100
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
@@ -16,7 +16,7 @@ global.loghandler = {
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
+        message: 'Service Unavailable, Under maintenance',
         creator: `${creator}`
     },
     apikey: {
@@ -26,7 +26,7 @@ global.loghandler = {
     },
     noturl: {
     	status: 403,
-    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	message: 'Forbidden, Invalid url, enter url parameters',
     	creator: `${creator}`,
     }
 }
